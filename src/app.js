791916a5dde604f54b22d3adf93f7862bf26ab7f
Jens Östlund <jens.ostlund@futurice.com>
2017-04-05 22:04:36 +0200

    Add multiple oscillators and oscillator controls

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-import {div, button, li, ul} from '@cycle/dom'
+import {div, button, input, label, li, option, select, ul} from '@cycle/dom'
 import xs from 'xstream'
+import sampleCombine from 'xstream/extra/sampleCombine'
 
 const F0 = 440; // A
+
+const OSCILLATOR_COUNT = 3;
 
 const START_STEP = 3; // C
 
@@ -9,17 +12,13 @@ const NOTE_STRINGS = [
   'A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#'
 ];
 
+const WAVEFORMS = ['sawtooth', 'triangle', 'square', 'sine'];
+
 const keyboardKeys = 'awsedftgyhujk';
 
 const audioContext = new AudioContext();
 
-const semitoneSteps = range(START_STEP, START_STEP + 12);
-
-function renderKey(step) {
-  const note = NOTE_STRINGS[step % 12];
-  const cssClass = note.match(/[b#]$/) ? '.sharp' : '';
-  return li(cssClass, button('.key', { attrs: { id: step } }, note))
-}
+const semitoneSteps = range(START_STEP, 12);
 
 export function App (sources) {
   const click$ = sources.DOM.select('.key')
@@ -30,9 +29,22 @@ export function App (sources) {
         .filter(i => i !== -1)
         .map(index => semitoneSteps[index])
 
-  const vtree$ = xs.merge(keydown$, click$)
-          .map(playNote).startWith(null)
-          .map(() => ul(semitoneSteps.map(renderKey)));
+  const note$ = xs.merge(keydown$, click$).map(toHertz);
+
+  const oscillators$ = xs.combine(
+    ...range(1, 3)
+      .map(id => sources.DOM.select(`#oscillator-${id}`))
+      .map(createOscillatorControlStream)
+  );
+
+  const vtree$ = note$.compose(sampleCombine(oscillators$))
+        .map(playNote).startWith(null)
+        .map(() =>
+             div([
+               div('.oscillators', range(1, 3).map(renderOscillator)),
+               ul(semitoneSteps.map(renderKey))
+             ])
+            );
 
   const sinks = {
     DOM: vtree$
@@ -43,17 +55,64 @@ export function App (sources) {
 
 // HELPER FUNCTIONS
 
-function playNote(semitoneStep) {
-  const note = toHertz(semitoneStep);
-  const osc = audioContext.createOscillator();
-  osc.frequency.value = note;
-  osc.connect(audioContext.destination);
-  osc.start(audioContext.currentTime);
-  osc.stop(audioContext.currentTime + 1);
+function createOscillatorControlStream(source) {
+  const waveform$ = source.select('.oscillator-controls__waveform').events('change')
+        .map(e => e.target.value)
+        .startWith(WAVEFORMS[0]);
+  const gain$ = source.select('.oscillator-controls__gain').events('click')
+        .map(e => e.target.value)
+        .startWith(0.5);
+  const detune$ = source.select('.oscillator-controls__detune').events('click')
+        .map(e => e.target.value)
+        .startWith(0);
+  return xs.combine(waveform$, gain$, detune$).map(([waveform, gain, detune]) => ({
+    waveform,
+    gain,
+    detune
+  }));
 }
 
-function range(start, end) {
-  return Array.from(new Array(end - start), (_, idx) => idx + start);
+function playNote([frequency, oscillators]) {
+  oscillators.map(({ gain, waveform, detune }) => {
+    const gainNode = audioContext.createGain();
+    gainNode.gain.value = gain;
+    gainNode.connect(audioContext.destination);
+
+    const oscillator = audioContext.createOscillator();
+    oscillator.type = waveform;
+    oscillator.detune.value = detune;
+    oscillator.frequency.value = frequency;
+    oscillator.connect(gainNode);
+    return oscillator;
+  }).forEach(oscillator => {
+    oscillator.start(audioContext.currentTime);
+    oscillator.stop(audioContext.currentTime + 1);
+  });
+}
+
+function range(start, count) {
+  return Array.from(new Array(count), (_, idx) => idx + start);
+}
+
+function renderKey(step) {
+  const note = NOTE_STRINGS[step % 12];
+  const cssClass = note.match(/[b#]$/) ? '.sharp' : '';
+  return li(cssClass, button('.key', { attrs: { id: step } }, note))
+}
+
+function renderOscillator(id) {
+
+  return div('.oscillator', { attrs: { id: `oscillator-${id}` } }, [
+    label('.oscillator__name', `OSC${id}`),
+    div('.oscillator-controls', [
+      label('Waveform'),
+      select('.oscillator-controls__waveform', WAVEFORMS.map((val, idx) => option({ attrs: { value: val }}, val))),
+      label('Gain'),
+      input('.oscillator-controls__gain', { attrs: { type: 'range', min: 0, max: 1, step: 0.01, value: 0.5 }}),
+      label('Detune'),
+      input('.oscillator-controls__detune', { attrs: { type: 'range', min: -100, max: 100, step: 1, value: 0 }})
+    ])
+  ]);
 }
 
 function toHertz(semitoneStep) {
