5d367955eb621b4b7499d2f5c87ee2ff8b22a004
Jens Östlund <jens.ostlund@futurice.com>
2017-04-14 23:44:38 +0200

    Makes playing multiple simultaneous notes possible on the keyboard

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-export const log = msg => x => console.log(msg || '', x);
-
 export const toHertz = f0 => semitoneStep => {
   const a = 2 ** (1/12);
   return f0 * (a ** semitoneStep);
@@ -9,3 +7,8 @@ export const classnames = (propsObj) =>
   Object.keys(propsObj)
   .reduce((classString, cls) => propsObj[cls] ? `${classString} ${cls}` : classString, '')
   .trim();
+
+export const diff = (list1, list2) => [
+  list1.filter(x => !list2.includes(x)),
+  list2.filter(x => !list1.includes(x))
+];
