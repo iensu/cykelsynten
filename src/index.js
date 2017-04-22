b0a98c4cf06ddd0d003458cd11ab3e9dd36e982b
Jens Östlund <jens.ostlund@futurice.com>
2017-04-22 23:58:34 +0200

    Replaces RAFDriver with @cycle/time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import {run} from '@cycle/run';
 import {makeDOMDriver} from '@cycle/dom';
+import {timeDriver} from '@cycle/time';
 import {App} from './app';
-import { makeWebAudioDriver } from './drivers/webAudioDriver';
-import { makeRAFDriver } from './drivers/requestAnimationFrameDriver';
-import { makeAudioCanvasDriver } from './drivers/audioCanvasDriver';
+import {makeWebAudioDriver} from './drivers/webAudioDriver';
+import {makeAudioCanvasDriver} from './drivers/audioCanvasDriver';
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const main = App;
@@ -11,7 +11,7 @@ const main = App;
 const drivers = {
   DOM: makeDOMDriver('#app'),
   Audio: makeWebAudioDriver(audioContext),
-  RAF: makeRAFDriver(),
+  Time: timeDriver,
   Canvas: makeAudioCanvasDriver('audio-canvas', 200, 170)
 };
 
